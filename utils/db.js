ed60ceb6fb4cd4f9099ed97eb119b54b939daf8e
nikitero <nikitavasiljevs@gmail.com>
2024-05-18 09:10:06 +0200

    test

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // URL local de nuestra base de datos en mongoose y su nombre upgrade_class_3
-const DB_URL = 'mongodb://0.0.0.0:27017/curriculum';
+const DB_URL = 'mongodb+srv://nikitero:Galactica21@nikitero.w9klsyr.mongodb.net/civify';
 
 // Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
 mongoose.connect(DB_URL, {});
