ab65076ed8ee48f3e61f5862973666e1423a8c64
nikitero <nikitavasiljevs@gmail.com>
2024-10-09 19:14:09 +0200

    adding DB_LOCAL variable to dotenv

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-// URL local de nuestra base de datos en mongoose y su nombre upgrade_class_3
-const DB_URL = process.env.DB_URL || 'mongodb://0.0.0.0:27017/curriculum';
+const DB_URL = process.env.DB_URL || process.env.DB_LOCAL;
 
-// Función que conecta nuestro servidor a la base de datos de MongoDB mediante mongoose
 const connect = () => mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
